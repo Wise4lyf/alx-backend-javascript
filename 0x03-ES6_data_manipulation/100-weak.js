c82c65edf0892ac1147d408b76ba22652f544d99
Onogwu Solomon Wisdom <113610190+Wise4lyf@users.noreply.github.com>
2023-08-15 12:56:12 +0100

    Update 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -14,7 +14,7 @@ const MAX_ENDPOINT_CALLS = 5;
  *   protocol: String,
  *   name: String,
  * }} endpoint - The endpoint to make a request to.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Onogwu Solomon <https://github.com/wise4lyf>
  */
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
