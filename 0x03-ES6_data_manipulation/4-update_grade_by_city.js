3b6769568743fe4425e30380924fe610e71c903d
Onogwu Solomon Wisdom <113610190+Wise4lyf@users.noreply.github.com>
2023-08-15 12:57:38 +0100

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -10,7 +10,7 @@
  *   studentId: Number,
  *   grade: Number,
  * }[]} newGrades - The new grades to be given to a student.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Onogwu Solomon <https://github.com/wise4lyf>
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function updateStudentGradeByCity(students, city, newGrades) {
